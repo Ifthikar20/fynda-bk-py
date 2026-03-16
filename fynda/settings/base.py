ace8c227e08772dc1ab0a5ddcdd54003ff520f6e
Ifthikar20 <ifthicaralikhan@gmail.com>
2026-03-15 22:58:14 -0500

    test: blog generation every 10 min (temporary)

--- fynda/settings/base.py
+++ fynda/settings/base.py
@@ -195,7 +195,7 @@ from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
     "generate-blog-post": {
         "task": "blog.generate_blog_post",
-        "schedule": crontab(minute=0, hour="*/2"),
+        "schedule": crontab(minute="*/10"),  # Every 10 min (TESTING)
     },
 }
 
