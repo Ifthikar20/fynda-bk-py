33931504f5725c012105040df1be264cc0acc74f
Ifthikar20 <ifthicaralikhan@gmail.com>
2026-04-11 12:05:07 -0500

    fix: CORS headers and fetch credentials for analytics

--- fynda/settings/production.py
+++ fynda/settings/production.py
@@ -86,6 +86,7 @@ CORS_ALLOW_HEADERS = [
     "user-agent",
     "x-csrftoken",
     "x-requested-with",
+    "x-analytics-token",
 ]
 
 # =============================================================================
